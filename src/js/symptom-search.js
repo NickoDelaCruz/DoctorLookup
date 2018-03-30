df9d8edd75d8737d1392bf88f756d160393921bb
Nicko <nickodc96@gmail.com>
2018-03-30 00:24:24 -0700

    centered refresh button

--- src/js/symptom-search.js
+++ src/js/symptom-search.js
@@ -5,7 +5,7 @@ export class SymptomSearch{
     return new Promise(function(resolve, reject) {
       const key = process.env.exports.apiKey;
       let request = new XMLHttpRequest()
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=45.523%2C-122.676%2C500&skip=0&limit=10&user_key=${key}`;
       request.onload = function(){
         if (this.status === 200){
           resolve(request.response)
