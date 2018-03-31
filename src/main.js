9aafd319e6ce521d52abc04509f14e817ef53f9f
Nicko <nickodc96@gmail.com>
2018-03-30 20:22:00 -0700

    add append loop for symp-search, into results ID

--- src/main.js
+++ src/main.js
@@ -16,17 +16,26 @@ $(document).ready(function() {
       let promise = newSymptomSearch.getDoctor(symptom);
       promise.then(function(response) {
         let SymptomSearchList = JSON.parse(response)
-        if (Object.keys(SymptomSearchList.data).length < 1) 
-        {
-         $("#results").append("<h1>NO RESULTS </h1>")
-       } else {
-         for (let 1 = 0; i < Object.keys(sickList.data).length; i++)
-         {
-          $("#results").append(
+        if(Object.keys(SymptomSearchList.data).length < 1) {
+          $("#results").append("<h1>NO RESULTS </h1>")
+        } else {
+          for(let i = 0; i < Object.keys(SymptomSearchList.data).length ; i++) {
+            $("#results").append(
+             "<ul> Speciality: " + SymptomSearchList.data[i].specialties[0].name + "</ul>"
+            + "<ul> Name: " + SymptomSearchList.data[i].profile.first_name + " " + SymptomSearchList.data[i].profile.last_name + "</ul>"
 
-          )
-         }
-       }
+            + "<ul> Address: " + SymptomSearchList.data[i].practices[0].visit_address.street + "</ul>"
+            + "<ul> City: " +   SymptomSearchList.data[i].practices[0].visit_address.city + " , "
+            + SymptomSearchList.data[i].practices[0].visit_address.state + " " + SymptomSearchList.data[i].practices[0].visit_address.zip + "</ul>"
+
+            + "<ul> CALL: " + SymptomSearchList.data[i].practices[0].phones[0].number + "</ul>"
+
+            + "<ul> Accepting New Patients: " + SymptomSearchList.data[i].practices[0].accepts_new_patients + "</ul>"
+            + "<ul> Website:" + SymptomSearchList.data[i].practices[0].website + "</ul>"
+
+            )
+          }
+        }
       }
 
 
